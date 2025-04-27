65fe65001ce0e5b913ae81743d1765bcb0eaa779
Stacy Callahan <callahanstacyr@gmail.com>
2025-04-27 03:32:49 -0700

    Updated ohio dataset pre-processing and model

--- src/ohio_bac_model.py
+++ src/ohio_bac_model.py
@@ -2,10 +2,10 @@ import pandas as pd
 ohio_bac_dataset = pd.read_csv('data/processed/ohio_bac_dataset_processed.csv')
 
 from sklearn.linear_model import LinearRegression
-X = ohio_bac_dataset[['Beers', 'Weight']]
+X = ohio_bac_dataset[['Ethanol_grams', 'Bodyweight_grams']]
 Y = ohio_bac_dataset['BAC']
 model = LinearRegression()
 model.fit(X, Y)
 
 import joblib
-joblib.dump(model, 'ohio_bac_model.pkl')
+joblib.dump(model, '../output/ohio_bac_model.pkl')
